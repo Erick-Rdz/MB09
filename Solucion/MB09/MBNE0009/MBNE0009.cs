e7bbd633008a7543b0979dff506b28206c5d093f
Reyna1730292 <36910549+Reyna1730292@users.noreply.github.com>
2022-09-09 12:30:15 -0500

    proyecto

---
+++ Solucion/MB09/MBNE0009/MBNE0009.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MBNE0009
+{
+    public class MBNE0009
+    {
+        public string BDMID { get; set; }
+        public string NUMTARJ { get; set; }
+        public string NUMCUEN { get; set; }
+        public string FECHINI { get; set; }
+        public string FECHFIN { get; set; }
+        public string NUMECEL { get; set; }
+        public string IDCELUL { get; set; }
+        public string LATITUD { get; set; }
+        public string LONGITD { get; set; }
+        public string CONTREG { get; set; }
+        public string DATPAG { get; set; }
+
+    }
+}
